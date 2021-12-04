4d0a184f083eea9a083ebd629025155d84f256ca
thvlt <thvlt@protonmail.com>
2021-12-04 15:43:20 +0000

    riscv

--- script/buildroot_apt.sh
+++ script/buildroot_apt.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 sudo apt update
-sudo apt install -y rsync cpio binutils patch bzip2 python binutils-riscv64-linux-gnu
+sudo apt install -y xsel rsync cpio binutils patch bzip2 python binutils-riscv64-linux-gnu
