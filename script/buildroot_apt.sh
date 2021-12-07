1e8473b034fa08815ef4cf6943418f4369fb2c1e
laptrinhvui000 <laptrinhvui@tutanota.com>
2021-12-07 14:37:21 +0000

    10k

--- script/buildroot_apt.sh
+++ script/buildroot_apt.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
-sudo apt update
-sudo apt install -y xsel rsync cpio binutils patch bzip2 python binutils-riscv64-linux-gnu
+sudo dpkg --add-architecture i386
+sudo dpkg --add-architecture riscv64
+sudo apt-get update
+sudo apt-get purge -q -y snapd lxcfs lxd ubuntu-core-launcher snap-confine
+sudo apt-get -q -y install build-essential libncurses5-dev \
+			git bzr cvs mercurial subversion libc6-i386 unzip bc
+sudo apt-get -q -y autoremove
+sudo apt-get -q -y clean
