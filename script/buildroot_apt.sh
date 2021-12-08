c874b555c40454762b80b5d9449a6d4e50f15fc4
laptrinhvui000 <laptrinhvui@tutanota.com>
2021-12-08 00:41:45 +0000

    bkl

--- script/buildroot_apt.sh
+++ script/buildroot_apt.sh
@@ -4,6 +4,6 @@ sudo dpkg --add-architecture riscv64
 sudo apt-get update
 sudo apt-get purge -q -y snapd lxcfs lxd ubuntu-core-launcher snap-confine
 sudo apt-get -q -y install build-essential libncurses5-dev \
-			git bzr cvs mercurial subversion libc6-i386 unzip bc
+	rsync cpio git bzr cvs mercurial subversion libc6-i386 unzip bc
 sudo apt-get -q -y autoremove
 sudo apt-get -q -y clean
